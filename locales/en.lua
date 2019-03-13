dd2361f85b3cea09d4bd7a5b2a545a2760df1df9
joebopie <joebopie@googlemail.com>
2019-03-13 04:14:04 +0000

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -8,7 +8,16 @@ Locales ['en'] = {
   ['weed_processingenough'] = 'you must have ~b~3x~s~ ~g~Cannabis~s~ in order to process.',
   ['weed_processed'] = 'you\'ve processed ~b~3x~s~ ~g~Cannabis~s~ to ~b~1x~s~ ~g~Marijuana~s~',
   ['weed_processingtoofar'] = 'the processing has been ~r~canceled~s~ due to you abandoning the area.',
-
+--coke
+  ['coke_pickupprompt'] = 'press ~INPUT_CONTEXT~ to harvest the ~g~Coke~s~ plant.',
+  ['coke_inventoryfull'] = 'you do not have any more inventory space for ~g~Coke leaves~s~.',
+  ['coke_processprompt'] = 'press ~INPUT_CONTEXT~ to start ~g~Process Coke~s~.',
+  ['coke_processingstarted'] = 'processing ~g~Coke leaves~s~ into ~g~Coke bag~s~...',
+  ['coke_processingfull'] = 'processing ~r~canceled~s~ due to full inventory!',
+  ['coke_processingenough'] = 'you must have ~b~3x~s~ ~g~Coke leaves~s~ in order to process.',
+  ['coke_processed'] = 'you\'ve processed ~b~3x~s~ ~g~Coke leaves~s~ to ~b~1x~s~ ~g~Coke bag~s~',
+  ['coke_processingtoofar'] = 'the processing has been ~r~canceled~s~ due to you abandoning the area.',
+	
   -- drug dealer
   ['dealer_prompt'] = 'press ~INPUT_CONTEXT~ to talk with the ~r~Drug Dealer~s~.',
   ['dealer_title'] = 'drug Dealer',
@@ -17,7 +26,7 @@ Locales ['en'] = {
   ['dealer_sold'] = 'you\'ve sold ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
 
   -- license
-  ['license_title'] = 'you are required to own a processing license to process this product, it can be bought here.',
+  ['license_title'] = 'you are required to pay a fee to process this product, you can pay here.',
   ['license_no'] = 'no',
   ['license_bought'] = 'you bought the ~b~%s~s~ for ~r~$%s~s~',
   ['license_bought_fail'] = 'you cannot afford an ~b~%s~s~!',
