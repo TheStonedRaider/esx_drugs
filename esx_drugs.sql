07edf90383154f8dd1ee0ad613186b9efc033d14
TheStonedRaider <joebopie@googlemail.com>
2019-03-13 05:12:50 +0000

    added coke items

--- esx_drugs.sql
+++ esx_drugs.sql
@@ -1,10 +1,14 @@
 USE `essentialmode`;
 
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-	('cannabis', 'Cannabis', 40, 0, 1),
-	('marijuana', 'Marijuana', 14, 0, 1)
+	('cannabis', 'Cannabis', 45, 0, 1),
+	('marijuana', 'Marijuana', 15, 0, 1),
+	('coke', 'Coke leaves', 45, 0, 1),
+	('cokebag', 'Bag of Cocaine', 15, 0, 1)
+
 ;
 
 INSERT INTO `licenses` (`type`, `label`) VALUES
-	('weed_processing', 'Weed Processing License')
+	('weed_processing', 'Weed Processing License'),
+	('coke_processing', 'Cocane Processing License')
 ;
