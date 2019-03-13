cd975a2159bbb4c346ad7be0811233f3de4e824a
joebopie <joebopie@googlemail.com>
2019-03-13 04:46:18 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -24,8 +24,8 @@ Config.GiveBlack = true -- give black money? if disabled it'll give regular cash
 Config.CircleZones = {
 	WeedField = {coords = vector3(310.91, 4290.87, 45.15), name = _U('blip_weedfield'), color = 25, sprite = 496, radius = 100.0},
 	WeedProcessing = {coords = vector3(2329.02, 2571.29, 46.68), name = _U('blip_weedprocessing'), color = 25, sprite = 496, radius = 100.0},
-	CokeField = {coords = vector3(310.91, 4290.87, 45.15), name = _U('blip_cokefield'), color = 25, sprite = 496, radius = 100.0},
-	CokeProcessing = {coords = vector3(2329.02, 2571.29, 46.68), name = _U('blip_cokeprocessing'), color = 25, sprite = 496, radius = 100.0},
+	CokeField = {coords = vector3(3263.95, 5520.87, 62.15), name = _U('blip_cokefield'), color = 25, sprite = 496, radius = 100.0},
+	CokeProcessing = {coords = vector3(-618.02, -1628.29, 32.18), name = _U('blip_cokeprocessing'), color = 25, sprite = 496, radius = 100.0},
 
 	DrugDealer = {coords = vector3(-1172.02, -1571.98, 4.66), name = _U('blip_drugdealer'), color = 6, sprite = 378, radius = 25.0},
 }
