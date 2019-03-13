ae2c94d56b998d177fa4ac9bf2a2bad5a2691724
TheStonedRaider <joebopie@googlemail.com>
2019-03-13 06:43:25 +0000

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -24,16 +24,20 @@ Locales ['en'] = {
   ['dealer_item'] = '$%s',
   ['dealer_notenough'] = 'you don\'t have enough of that to sell!',
   ['dealer_sold'] = 'you\'ve sold ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
+  ['act_imp_police'] = 'Not enough police on the server-',
 
   -- license
   ['license_title'] = 'you are required to pay a fee to process this product, you can pay here.',
   ['license_no'] = 'no',
   ['license_bought'] = 'you bought the ~b~%s~s~ for ~r~$%s~s~',
   ['license_bought_fail'] = 'you cannot afford an ~b~%s~s~!',
-  ['license_weed'] = 'weed Processing License',
-
+  ['license_weed'] = 'weed Processing Deal',
+  ['license_coke'] = 'coke Processing Deal',
   -- blips
   ['blip_weedfield'] = 'weed Field',
   ['blip_weedprocessing'] = 'weed Processing',
+  ['blip_cokefield'] = 'Coke Field',
+  ['blip_cokeprocessing'] = 'Coke Processing',
   ['blip_drugdealer'] = 'drug Dealer',
+  ['Bigblip_drugdealer'] = 'Big drug Dealer',
 }
